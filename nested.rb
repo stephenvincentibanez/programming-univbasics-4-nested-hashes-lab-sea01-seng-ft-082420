7e29f1a58e14df86266f5732798c0bcab27660d7
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-06 05:14:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,11 +14,12 @@ def hopper
         :known_for => "Unix",
         :languages => ["C"]
       }
+      programmer_hash[:grace_hopper]
     }
 
 end
 
-programmer_hash[:grace_hopper]
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
