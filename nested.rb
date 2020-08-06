60b73f0c8d6d8b233b49c683f20b53fdf2ce134d
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-06 05:29:21 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,11 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
 
 end
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -36,6 +39,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +59,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,7 +86,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] ={
+:known_for => "Ruby", 
+:languages => ["LISP", "C"]
+}
+programmer_hash
 end
 
 def changing_alan
@@ -103,7 +112,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]="GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +135,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+programmer_hash
 end
